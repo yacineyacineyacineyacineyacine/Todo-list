fca0399eed990aee850c02afe8026eef15d44004
yacine <yacineyacine31212@gmail.com>
2019-02-12 17:10:12 +0100

    final final version

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
 //jshint esversion: 6
 const express = require('express');
 const bodyParser = require('body-parser');
-let app = express();
+const date = require(__dirname + '/date.js');
+const app = express();
 app.use(express.static(__dirname + '/public/'));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-let items =['Buy Food', 'Cook Food', 'Eat Food'] ;
-let workItems = [];
+const items =['Buy Food', 'Cook Food', 'Eat Food'] ;
+const workItems = [];
+
 app.get('/', function(req,res){
-    let today = new Date(); 
-    let options = {
-        weekday : 'long',
-        day:'numeric',
-        month: 'long'
-    }
-    let day = today.toLocaleDateString("en-US", options);
+    const day = date.getDate();
     res.render('list', { ListTitle: day, newListItems: items});
     });
+
+
 app.post('/', function(req,res){
-    let item = req.body.newItem;
+    const item = req.body.newItem;
     if (req.body.list === 'Work') {
         workItems.push(item);
         res.redirect('/work')
